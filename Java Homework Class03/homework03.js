d6b121a15e1f28aa1bdaf6d23cd0f690af8170e9
Tomeski05 <76494449+Tomeski05@users.noreply.github.com>
2021-01-18 16:17:08 +0100

    Add files via upload

---
+++ Java Homework Class03/homework03.js
@@ -0,0 +1,42 @@
+//Homework Part 1
+function checkOfType(x) {
+    console.log(typeof x);
+}
+
+checkOfType([1,2,3,4,5,6,7,8,9]);
+checkOfType(10);
+checkOfType("Homework");
+checkOfType(true);
+checkOfType();
+
+
+//Homework part 3
+
+// function banksum (x) {
+//     if ( x > 100000) {
+//         return console.log("Not enough Money");
+//     }
+//     else { 
+//         return console.log("your withraw is", x , "money left", 100000 - x);
+//     }
+// }
+// let inputUser = prompt("Enter your sum");
+// let converted = parseInt(inputUser);
+// let input = banksum(converted);
+
+
+// Homework Part 2
+
+// function dogYear(x) {
+//     let resultdogYear = x * 7;
+//     return resultdogYear;
+// }
+// let dogYears = dogYear(10);
+// console.log(dogYears);
+
+// function humanYear(y) {
+//     let resulthumanYear = y / 7;
+//     return resulthumanYear;
+// }
+// let humanYears = humanYear(20);
+// console.log(humanYears);
